dce70f9f62376682689db1085e893be10480cde1
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2022-08-24 20:17:34 +0900

    10 tests

---
+++ e2e/example6.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+
+test('Example 6', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+
+  const button = page.locator('data-testid=Button');
+  const text = page.locator('data-testid=Text');
+
+  await button.click();
+
+  await expect(text).toBeVisible();
+});
