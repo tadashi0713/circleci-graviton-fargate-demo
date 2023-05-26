9d463ea87799e891170cd876bdd7754e4246e408
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2023-05-26 13:46:24 +0900

    not use start-server-and-test

--- e2e/example6.spec.ts
+++ e2e/example6.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('Example 6', async ({ page }) => {
-  await page.goto('http://localhost:3000');
+  await page.goto('/');
 
   const button = page.locator('data-testid=Button');
   const text = page.locator('data-testid=Text');
